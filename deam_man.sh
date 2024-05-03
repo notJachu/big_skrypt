aa78ba4b32b98882747efccc7863bc435d32668e
notJachu <jasiustasiu50@interia.pl>
2024-05-03 20:43:29 +0200

    fixed listing running deamons

--- deam_man.sh
+++ deam_man.sh
@@ -17,18 +17,70 @@ function get_running_deamons {
     ps -eo 'tty,pid,comm,pcpu,pmem' | grep ^? | awk '{print $2, $3, $4, $5}'
 }
 
+function display_running {
+    local tmp_file="/tmp/running_daemons.txt"
+    get_running_deamons > "$tmp_file"
+    deamons_array=()
+    while IFS= read -r line; do
+        deamons_array+=("$line")
+    done < "$tmp_file"
+    #printf "%s\n" "${deamons_array[@]}"
+    zenity --list --column=PID --column=Name --column=CPU --column=Memory\
+    --text="Running deamons" --title="Deamons" --width=400 --height=300 \
+    --separator=" " ${deamons_array[@]}
+    
+    rm "$tmp_file" # Cleanup temporary file
+}
+
+
 function schedule_closing {
     echo "kill  $1" | at now + $2
 }
 
-function display_running {
-    get_running_deamons | zenity --list --column=PID --column=Name --column=CPU --column=Memory --checklist --separator=" " --text="Select deamons to close" --title="Deamons" --width=400 --height=300
+function close_deamons {
+    while read line; do
+        pid=$(echo $line | awk '{print $1}')
+        schedule_closing $pid 1 minute
+    done
+}
+
+function list_avaliable {
+    ls /etc/init.d/ | grep -v README | zenity --list --column=Name --text="Select deamon to start" --title="Deamons" --width=400 --height=300
+}
+
+function draw_start_menu {
+    zenity --list --column=Action --column=Description --text="Select action" --title="Deamons" --width=400 --height=300 \
+        "Display" "Display running deamons" \
+        "Close" "Close selected deamons"
+
+}
+
+function get_process_details {
+    ps -p $1 -o pid,comm,pcpu,pmem,time | zenity --text-info --title="Process details" --width=400 --height=300
+    # ps -p 37 -o pid,comm,pcpu,pmem,time
+    # $1 is pid
 }
 
 function main {
     get_running_deamons > tmp.txt
     check_dependencies
     zenity --info --text="Hello, world!"
+
+    display_running
+
+    while [1 -eq 1]; do
+        case $(draw_start_menu) in
+            "Display")
+                display_running < tmp.txt
+                ;;
+            "Close")
+                close_deamons < tmp.txt
+                ;;
+            *)
+                break
+                ;;
+        esac
+    done
 }
 
 main
