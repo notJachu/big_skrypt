e24f8e797e20587c9949a3c1d12a055ce3a350ce
notJachu <jasiustasiu50@interia.pl>
2024-05-04 17:00:56 +0200

    fixed checklist and closing deamons

--- deam_man.sh
+++ deam_man.sh
@@ -22,14 +22,24 @@ function display_running {
     get_running_deamons > "$tmp_file"
     deamons_array=()
     while IFS= read -r line; do
-        deamons_array+=("$line")
+        deamons_array+=("no $line") # adding "no" fixes wierd zenity checkbox that must steal one column from data
     done < "$tmp_file"
     #printf "%s\n" "${deamons_array[@]}"
-    zenity --list --column=PID --column=Name --column=CPU --column=Memory\
+    # res=$(zenity --list --checklist --column=PID --column=Name --column=CPU --column=Memory \
+    # --text="Running deamons" --title="Deamons" --width=400 --height=300 \
+    # --separator=" " ${deamons_array[@]})
+
+    res=$(zenity --list --checklist --column=Selected --column=PID --column=Name --column=CPU --column=Memory\
     --text="Running deamons" --title="Deamons" --width=400 --height=300 \
-    --separator=" " ${deamons_array[@]}
-    
+    --separator=" " ${deamons_array[@]})
     rm "$tmp_file" # Cleanup temporary file
+
+    if [ -n "$res" ]; then
+        for pid in $res; do
+            echo $pid
+            #get_process_details $pid
+        done
+    fi
 }
 
 
@@ -38,10 +48,31 @@ function schedule_closing {
 }
 
 function close_deamons {
-    while read line; do
-        pid=$(echo $line | awk '{print $1}')
-        schedule_closing $pid 1 minute
-    done
+    # while read line; do
+    #     echo $line | awk '{print $1}'
+    #     #pid=$(echo $line | awk '{print $1}')
+    #     #echo $pid
+    #     schedule_closing $pid "1 minute"
+    # done
+    local tmp_file="/tmp/running_daemons.txt"
+    get_running_deamons > "$tmp_file"
+    deamons_array=()
+    while IFS= read -r line; do
+        deamons_array+=("no $line") # adding "no" fixes wierd zenity checkbox that must steal one column from data
+    done < "$tmp_file"
+
+    res=$(zenity --list --checklist --column=Selected --column=PID --column=Name --column=CPU --column=Memory\
+    --text="Select deamons to be closed in 1 minute" --title="Deamons" --width=400 --height=300 \
+    --separator=" " ${deamons_array[@]})
+    rm "$tmp_file" # Cleanup temporary file
+
+    if [ -n "$res" ]; then
+        for pid in $res; do
+            echo $pid
+            schedule_closing $pid "1 minute"
+        done
+    fi
+
 }
 
 function list_avaliable {
@@ -51,7 +82,7 @@ function list_avaliable {
 function draw_start_menu {
     zenity --list --column=Action --column=Description --text="Select action" --title="Deamons" --width=400 --height=300 \
         "Display" "Display running deamons" \
-        "Close" "Close selected deamons"
+        "Close all" "Close all running deamons in 1 minute (may crash system)"
 
 }
 
@@ -62,14 +93,17 @@ function get_process_details {
 }
 
 function main {
-    get_running_deamons > tmp.txt
     check_dependencies
-    zenity --info --text="Hello, world!"
+    #get_running_deamons > tmp.txt
 
-    display_running
+    #display_running
+    #get_process_details 37
 
-    while [1 -eq 1]; do
-        case $(draw_start_menu) in
+    list_avaliable
+
+    while [ 1 -eq 1 ]; do
+        res=`draw_start_menu`
+        case "$res" in
             "Display")
                 display_running < tmp.txt
                 ;;
@@ -77,6 +111,7 @@ function main {
                 close_deamons < tmp.txt
                 ;;
             *)
+                exit 0
                 break
                 ;;
         esac
